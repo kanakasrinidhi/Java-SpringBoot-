7c8cf43bbfbbc833582cf4c45a7553fdeb818804
[Kanaka Srinidhi] <[bkanakasrinidhi@gmail.com]>
2025-05-20 05:35:00 +0530

    add files

---
+++ Library/library.java
@@ -0,0 +1,39 @@
+package com.example.library;
+
+
+public class library {
+    private String title;
+    private String author;
+    private double price;
+
+    public library() {}
+    public library(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
